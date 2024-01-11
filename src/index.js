b4266e16de39d2341baa554b6d1486eb157727a8
s890533 <s890533@gmail.com>
2024-01-12 00:07:53 +0800

    enable PWA

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 
 
@@ -23,4 +24,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
+serviceWorker.register();
